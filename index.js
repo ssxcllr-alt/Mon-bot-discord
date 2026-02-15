7f9b1fdd1b89cc5ed35e73589f563e2e46e11dbb
ssxcllr-alt <ssxcllr@gmail.com>
2026-02-16 00:42:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-);
+require('dotenv').config();
 const { Client, GatewayIntentBits, Collection, PermissionsBitField } = require('discord.js');
 const fs = require('fs');
 const DataSaver = require('./dataSaver');
